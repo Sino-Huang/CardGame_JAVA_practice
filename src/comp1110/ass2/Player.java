a555f1bf4b62a8adb2b6ac36805249aa3717dfb8
SUKAI at home <u6492211@anu.edu.au>
2018-03-18 12:09:42 +1100

    add Player Class, add skeleton for task 9, 10, 11

---
+++ src/comp1110/ass2/Player.java
@@ -0,0 +1,11 @@
+package comp1110.ass2;
+
+public class Player {
+    private String name;
+    private int position;
+
+    public Player(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+}
--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,5 +1,6 @@
 package comp1110.ass2.gui;
 
+import comp1110.ass2.Player;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,10 +9,19 @@ public class Game extends Application {
     private static final int BOARD_HEIGHT = 700;
 
     // FIXME Task 9: Implement a basic playable Warring States game in JavaFX
+    public void inputPlayer(){ // input numbers of players
+
+    }
 
     // FIXME Task 11: Allow players of your Warring States game to play against your simple agent
+    public char simpleMove (){ // may directly use task 10's method
+        return '\0';
+    }
 
     // FIXME Task 12: Integrate a more advanced opponent into your game
+    public char alphaBetaPruning (int node, int depth, int alpha, int beta, Player maximizingPlayer){ //use alpha beta pruning to get intelligent move
+        return '\0';
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
