04902ee05f3792155ebe0ac077c4a9fcec95f48c
SUKAI at home <u6492211@anu.edu.au>
2018-04-06 14:03:38 +1000

    edit Player class

--- src/comp1110/ass2/Player.java
+++ src/comp1110/ass2/Player.java
@@ -1,11 +1,54 @@
 package comp1110.ass2;
 
+import comp1110.ass2.gui.Characters;
+
+import java.util.HashSet;
+
+enum Flags {
+    A,B,C,D,E,F,G;
+
+    private String name;
+
+    Flags() {
+        this.name = this.name();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
 public class Player {
     private String name;
     private int position;
+    private HashSet<Characters> cards;
+    private HashSet<Flags> flags;
+    private double score;
 
     public Player(String name, int position) {
         this.name = name;
         this.position = position;
+    }
+
+    public void setCards(String cards) {
+        for (int i = 0; i < cards.length(); i += 2) {
+            for (Characters characters : Characters.values()) {
+                if (characters.name().equals(cards.substring(i, i + 2))) {
+                    this.cards.add(characters);
+                }
+            }
+        }
+    }
+
+    public void setFlags(String flags) {
+        for (Flags flag : Flags.values()) {
+            if (flag.getName().equals(flags)) {
+                this.flags.add(flag);
+            }
+        }
+    }
+
+    public void setScore() {
+        this.score = flags.length * 10 + cards.length * 0.1;
     }
 }
--- src/comp1110/ass2/gui/Characters.java
+++ src/comp1110/ass2/gui/Characters.java
@@ -7,6 +7,7 @@ public enum Characters {
     A0,A1,A2,A3,A4,A5,A6,A7,B0,B1,B2,B3,B4,B5,B6,C0,C1,C2,C3,C4,C5,D0,D1,D2,D3,D4,E0,E1,E2,E3,F0,F1,F2,G0,G1,Z9;
     public final ImageView imageView;
 
+
     Characters() {
         this.imageView = new ImageView(new Image(getClass().getResourceAsStream("assets/" + this.name() + ".png")));
     }
